88b6fd28cf5489c73af6f456bb895529be2eee00
jP Balderas <jpbalderas17@yahoo.com>
2015-04-02 23:44:54 +0800

    Part Two is Through
    Javascript OOP basics.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function Main () {
+	this.stage=new PIXI.Stage(0x66FF99);
+	this.renderer= PIXI.autoDetectRenderer(
+		512,
+		384,
+		{view:document.getElementById("game-canvas")}
+		);
+	this.scroller = new Scroller(this.stage);
+
+
+	requestAnimFrame(this.update.bind(this));
+}
+Main.SCROLL_SPEED=3;
+Main.prototype.update=function() {
+	this.scroller.moveViewportXBy(Main.SCROLL_SPEED);
+	this.renderer.render(this.stage);
+	requestAnimFrame(this.update.bind(this));
+}
---
+++ js/scroller.js
@@ -0,0 +1,27 @@
+function Scroller (stage) {
+	this.far=new Far();
+	stage.addChild(this.far);
+
+	this.mid=new Mid();
+	stage.addChild(this.mid);
+
+	this.viewportX=0;
+}
+
+Scroller.prototype.update=function(){
+	this.far.update();
+	this.mid.update();
+
+};
+Scroller.prototype.setViewportX=function (viewportX) {
+	this.viewportX=viewportX;
+	this.far.setViewportX(viewportX);
+	this.mid.setViewportX(viewportX);
+}
+Scroller.prototype.getViewportX=function(){
+	return this.viewportX;
+}
+Scroller.prototype.moveViewportXBy=function (units) {
+	var newViewPortX = this.viewportX + units;
+	this.setViewportX(newViewPortX);
+}
